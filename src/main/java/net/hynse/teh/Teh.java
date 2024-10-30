830c393e3232cbc3c01254e308e8f3e83cc29556
MidnightTale <midnighttale24@gmail.com>
2024-10-30 10:05:08 +0700

    w

--- src/main/java/net/hynse/teh/Teh.java
+++ src/main/java/net/hynse/teh/Teh.java
@@ -13,6 +13,8 @@ import org.bukkit.util.Transformation;
 import org.joml.Vector3f;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.md_5.bungee.api.chat.hover.content.Text;
+
 import java.util.Random;
 import org.joml.Matrix4f;
 
@@ -27,49 +29,185 @@ public final class Teh extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    void onEntityDamage(EntityDamageEvent event) {
-        Entity entity = event.getEntity();
-        double damage = event.getFinalDamage();
+    void onEntityDamage(EntityDamageEvent e) {
+        Entity entity = e.getEntity();
+        double damage = e.getFinalDamage();
         
-        Location loc = entity.getLocation().add(
+        Location baseLocation = entity.getLocation();
+        Location displayLocation = baseLocation.clone().add(
             (random.nextDouble() - 0.5) * 0.5,
             1.5,
             (random.nextDouble() - 0.5) * 0.5
         );
 
-        // Spawn and configure text display
-        TextDisplay textDisplay = entity.getWorld().spawn(loc, TextDisplay.class);
-        textDisplay.text(Component.text(String.format("%.1f", damage))
-            .color(NamedTextColor.RED));
-        textDisplay.setSeeThrough(true);
-        textDisplay.setPersistent(false);
-        textDisplay.setBackgroundColor(Color.fromARGB(0, 0, 0, 0));
-        textDisplay.setAlignment(TextDisplay.TextAlignment.CENTER);
-        textDisplay.setBillboard(Display.Billboard.CENTER);
-
-        Matrix4f scale = new Matrix4f().scale(0.0f);
-        Matrix4f scale2 = new Matrix4f().scale(1.3f);
-        Matrix4f scale3 = new Matrix4f().scale(0.0f);
-
-        textDisplay.setInterpolationDelay(0);
-        textDisplay.setTransformationMatrix(scale);
-
+        TextDisplay display = createDamageDisplay(entity, displayLocation, damage);
+        getLogger().info("display created");
+        // First animate in
+        animateEaseIn(display, 1.3f, 8, 3);
+        
+        // Then schedule the fade out animation after a delay
         getServer().getScheduler().runTaskLater(this, () -> {
-            textDisplay.setInterpolationDuration(8); 
-            textDisplay.setTransformationMatrix(scale2);
-        }, 3L);
-        // getServer().getScheduler().runTaskLater(this, () -> {
-        //     textDisplay.setInterpolationDuration(20 * 4); 
-        //     textDisplay.setTransformationMatrix(scale3);
-        // },20L * 6L);
+            animateEaseOut(display, 1.3f, 16, 6);
+        }, 9L); // Wait 1.5 seconds before starting fade out
+        getLogger().info("fade out scheduled");
+    }
+        /**
+     * Creates a configured TextDisplay entity for damage numbers
+     * @param entity Source entity for the world reference
+     * @param location Location to spawn the display
+     * @param damage Damage amount to display
+     * @return Configured TextDisplay entity
+     */
+    private TextDisplay createDamageDisplay(Entity entity, Location location, double damage) {
+        TextDisplay display = entity.getWorld().spawn(location, TextDisplay.class);
+        
+        // Configure text and appearance
+        display.text(Component.text(String.format("%.1f", damage))
+            .color(NamedTextColor.RED));
+        display.setSeeThrough(true);
+        display.setPersistent(false);
+        display.setBackgroundColor(Color.fromARGB(0, 0, 0, 0));
+        display.setAlignment(TextDisplay.TextAlignment.CENTER);
+        display.setBillboard(Display.Billboard.CENTER);
+        display.setTransformationMatrix(new Matrix4f().scale(0.0f));
+        
+        return display;
     }
 
-    //     // Remove the text display after 2 seconds
-    //     getServer().getScheduler().runTaskLater(this, () -> {
-    //         textDisplay.remove();
-    //     }, 40L); // 40 ticks = 2 seconds
-    // }
+    /**
+     * Animates a Display entity with dynamic ease-in scaling
+     * @param display The Display entity to animate
+     * @param targetScale The final scale to reach
+     * @param durationTicks Total duration in ticks (20 ticks = 1 second)
+     * @param steps Number of animation steps to use
+     */
+    public void animateEaseIn(Display display, float targetScale, int durationTicks, int steps) {
+        getLogger().info("animateEaseIn");
+        // Calculate time between steps
+        int stepDuration = Math.max(1, durationTicks / steps);
+        
+        // Calculate scale values using easeOutQuad function
+        float[] scaleSteps = calculateEaseOutScales(0.0f, targetScale, steps);
+        
+        // Calculate interpolation duration for smooth transitions
+        int interpolationDuration = Math.max(1, stepDuration / 2);
+        
+        BukkitRunnable animation = new BukkitRunnable() {
+            private int currentStep = 0;
+            
+            @Override
+            public void run() {
+                if (currentStep >= steps) {
+                    this.cancel();
+                    // Remove display after animation completes
+                    getServer().getScheduler().runTaskLater(
+                        Teh.this, 
+                        display::remove, 
+                        20L // Keep visible for 1 second after animation
+                    );
+                    return;
+                }
+                
+                display.setInterpolationDuration(interpolationDuration);
+                display.setInterpolationDelay(0);
+                display.setTransformationMatrix(
+                    new Matrix4f().scale(scaleSteps[currentStep])
+                );
+                getLogger().info("scale" + currentStep + " = " + scaleSteps[currentStep]);
+                
+                currentStep++;
+            }
+        };
+        
+        // Start animation with calculated timing
+        animation.runTaskTimer(this, 2L, stepDuration);
+    }
 
+    /**
+     * Calculates scale values using easeOutQuad easing function
+     * @param startScale Starting scale value
+     * @param endScale Target scale value
+     * @param steps Number of steps to calculate
+     * @return Array of scale values
+     */
+    private float[] calculateEaseOutScales(float startScale, float endScale, int steps) {
+        float[] scales = new float[steps];
+        float change = endScale - startScale;
+        
+        for (int i = 0; i < steps; i++) {
+            float progress = (float) i / (steps - 1);
+            // EaseOutQuad formula: 1 - (1 - progress) * (1 - progress)
+            float easedProgress = 1 - (1 - progress) * (1 - progress);
+            scales[i] = startScale + (change * easedProgress);
+        }
+        
+        return scales;
+    }
+
+    /**
+     * Animates a Display entity with dynamic ease-out scaling (shrinking effect)
+     * @param display The Display entity to animate
+     * @param startScale The initial scale to start from
+     * @param durationTicks Total duration in ticks (20 ticks = 1 second)
+     * @param steps Number of animation steps to use
+     */
+    public void animateEaseOut(Display display, float startScale, int durationTicks, int steps) {
+        getLogger().info("animateEaseOut");
+        // Calculate time between steps
+        int stepDuration = Math.max(1, durationTicks / steps);
+        
+        // Calculate scale values using easeInQuad function (reverse of easeOutQuad)
+        float[] scaleSteps = calculateEaseInScales(startScale, 0.0f, steps);
+        
+        // Calculate interpolation duration for smooth transitions
+        int interpolationDuration = Math.max(1, stepDuration / 2);
+        
+        BukkitRunnable animation = new BukkitRunnable() {
+            private int currentStep = 0;
+            
+            @Override
+            public void run() {
+                if (currentStep >= steps) {
+                    this.cancel();
+                    display.remove();
+                    return;
+                }
+                
+                display.setInterpolationDuration(interpolationDuration);
+                display.setInterpolationDelay(0);
+                display.setTransformationMatrix(
+                    new Matrix4f().scale(scaleSteps[currentStep])
+                );
+                getLogger().info("scale" + currentStep + " = " + scaleSteps[currentStep]);
+                currentStep++;
+            }
+        };
+        
+        // Start animation with calculated timing
+        animation.runTaskTimer(this, 2L, stepDuration);
+    }
+
+    /**
+     * Calculates scale values using easeInQuad easing function
+     * Creates a shrinking effect from start to end
+     * @param startScale Starting scale value
+     * @param endScale Target scale value (usually 0)
+     * @param steps Number of steps to calculate
+     * @return Array of scale values
+     */
+    private float[] calculateEaseInScales(float startScale, float endScale, int steps) {
+        float[] scales = new float[steps];
+        float change = endScale - startScale;
+        
+        for (int i = 0; i < steps; i++) {
+            float progress = (float) i / (steps - 1);
+            // EaseInQuad formula: progress * progress
+            float easedProgress = progress * progress;
+            scales[i] = startScale + (change * easedProgress);
+        }
+        
+        return scales;
+    }
 
     @Override
     public void onDisable() {
